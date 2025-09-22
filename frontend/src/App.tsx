66d7860de6177c2a0352c667942dd538258461c4
Isaac Restrick <isaac.restrick@gmail.com>
2025-09-22 11:30:18 -0400

    Initial board

---
+++ frontend/src/App.tsx
@@ -0,0 +1,44 @@
+function App() {
+    return (
+      <div>
+        <h1>Tic Tac Toe Board:</h1>
+        <table>
+          <tr>
+            <td>
+              X
+            </td>
+            <td>
+              O
+            </td>
+            <td>
+              X
+            </td>
+          </tr>
+          <tr>
+            <td>
+              X
+            </td>
+            <td>
+              O
+            </td>
+            <td>
+              X
+            </td>
+          </tr>
+          <tr>
+            <td>
+              X
+            </td>
+            <td>
+              O
+            </td>
+            <td>
+              X
+            </td>
+          </tr>
+        </table>
+      </div>
+    )
+}
+
+export default App
