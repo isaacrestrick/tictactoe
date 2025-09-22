0849fbbb3a0f68053d553806fbd7cd145690b6ee
Isaac Restrick <isaac.restrick@gmail.com>
2025-09-22 17:45:57 -0400

    blah

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,10 @@ import { countEmptyCells, makeMove, startingGame} from '/src/tictactoe.ts'
 
 const App = () => {
     const [game, setGame] = useState(startingGame)
-
+    const [playingAgainstSelf, setPlayingAgainstSelf] = useState(true)
+    const [humanScoreCard, setHumanScoreCard] = useState([0, 0])
+    const [aiScoreCard, setAIScoreCard] = useState([0, 0])
+  
     const clickCell = (row: number, col: number) => {
         setGame(makeMove(game, row, col))
       
@@ -56,11 +59,42 @@ const App = () => {
       )
     }
 
+    const ScoreCard = () => {
+
+      return (
+      <table className="mx-auto">
+        <thead>
+          <tr>
+            <td className="p-1 text-1xl" >Scores vs {playingAgainstSelf ? "self": "AI"}</td>
+            <td className="p-1 text-1xl">X</td>
+            <td className="p-1 text-1xl">O</td>
+          </tr>
+        </thead>
+        <tbody className="border border-black">
+          <tr>
+            <td className="p-1 text-1xl">Totals</td>
+            <td className="p-1 text-1xl">{playingAgainstSelf ? humanScoreCard[0]: aiScoreCard[0]}</td>
+            <td className="p-1 text-1xl">{playingAgainstSelf ? humanScoreCard[1]: aiScoreCard[1]}</td>
+          </tr>
+        </tbody>
+      </table>
+      )
+    }
+
+    const PlayingAgainstToggle = () => {
+      return(
+      <div>
+        <p className="text-center">toggle: <button className="border border-black py-1 px-2" onClick={()=>{setPlayingAgainstSelf(!playingAgainstSelf)}}>{playingAgainstSelf ? "playing against self" : "playing against AI"}</button></p>
+      </div>)
+    }
+
     return (
       <div className="flex flex-col space-y-6">
         <h1 className="m-6 text-3xl font-bold underline text-center">Tic Tac Toe Board:</h1>
         <GameMessage />
         <TicTacToeBoard />
+        <PlayingAgainstToggle />
+        <ScoreCard />
       </div>
     )
 }
